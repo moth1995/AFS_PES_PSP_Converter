e615324f865325db4c553b23ba306abfb74a9e5f
moth1995 <marcos-flores@live.com.ar>
2022-01-22 08:05:21 -0300

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -7,9 +7,9 @@ class Main:
         my_afs.save_file("new_over.afs")
 
         # Ejemplo de como convertir de afs generico a afs psp
-        #my_afs = AFS("new_over.afs",False)
-        #my_afs.convert_to_psp()
-        #my_afs.save_file("new_over2.afs")
+        my_afs = AFS("new_over.afs",False)
+        my_afs.convert_to_psp()
+        my_afs.save_file("new_over2.afs")
 
 if __name__ == "__main__":
     Main()
